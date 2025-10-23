6c5e9e12f83288a4a46a194622a16e9945d2eca3
FernandaCamacho <126592924+FernandaGarciaCamacho@users.noreply.github.com>
2025-10-22 19:50:55 -0600

    Update 02_query_data.sql

--- 02_query_data.sql
+++ 02_query_data.sql
@@ -3,3 +3,9 @@ COPY datos (id, categoria, valor, fecha)
 FROM 'datos_agregacion_transformacion.csv'
 DELIMITER ','
 CSV HEADER;
+
+
+SELECT * FROM crosstab(
+  'SELECT categoria, SUM(valor) AS valor FROM datos GROUP BY categoria',
+  'SELECT DISTINCT categoria, SUM(valor) FROM datos ORDER BY categoria'
+) AS ct (id_fila INT, columna_1 VARCHAR, columna_2 VARCHAR);
