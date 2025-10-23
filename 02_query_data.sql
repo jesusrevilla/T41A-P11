0b14f439247e6d8414fa6ccaa7f3e7a50d645f10
Rafael López <126181633+Refex38@users.noreply.github.com>
2025-10-22 19:50:41 -0600

    Update 02_query_data.sql

--- 02_query_data.sql
+++ 02_query_data.sql
@@ -1,1 +1,7 @@
-
+SELECT
+    SUM(CASE WHEN categoria = 'A' THEN valor ELSE 0 END) AS "A",
+    SUM(CASE WHEN categoria = 'B' THEN valor ELSE 0 END) AS "B",
+    SUM(CASE WHEN categoria = 'C' THEN valor ELSE 0 END) AS "C",
+    SUM(CASE WHEN categoria = 'D' THEN valor ELSE 0 END) AS "D"
+FROM
+    datos;
