2a1bbc52d7e39e34a3095804c856483e858a0a6e
Coral024 <202579467+Coral024@users.noreply.github.com>
2025-10-22 19:49:54 -0600

    Update 02_query_data.sql

--- 02_query_data.sql
+++ 02_query_data.sql
@@ -1,1 +1,12 @@
-
+SELECT 
+    categoria,
+    SUM(CASE WHEN categoria = 'A' THEN valor ELSE 0 END) AS ValorA, 
+    SUM(CASE WHEN categoria = 'B' THEN valor ELSE 0 END) AS ValorB,
+    SUM(CASE WHEN categoria = 'C' THEN valor ELSE 0 END) AS ValorC
+    SUM(CASE WHEN categoria = 'D' THEN valor ELSE 0 END) AS ValorD, 
+FROM 
+    datos
+GROUP BY 
+    categoria
+ORDER BY 
+    categoria; 
