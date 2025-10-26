45d725777eaf044769c745ec27c2df883302a525
Bryan Yareth Gámez Contreras <96131248+BryanYarethGC@users.noreply.github.com>
2025-10-26 17:54:26 -0600

    Update insertar_datos.sql

--- insertar_datos.sql
+++ insertar_datos.sql
@@ -1,3 +1,1 @@
-\copy datos(id, categoria, valor, fecha) 
-FROM 'datos_agregacion_transformacion.csv' 
-WITH (FORMAT CSV, HEADER TRUE);
+\copy datos(id, categoria, valor, fecha) FROM 'datos_agregacion_transformacion.csv' WITH (FORMAT CSV, HEADER TRUE);
