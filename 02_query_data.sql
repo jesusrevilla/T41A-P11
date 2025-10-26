b67d76d132dab088064523cc5b7f0e82d68278ea
Bryan Yareth Gámez Contreras <96131248+BryanYarethGC@users.noreply.github.com>
2025-10-26 17:31:50 -0600

    Add query to sum values by category
    Added a query to sum values by category A, B, C, and D.

--- 02_query_data.sql
+++ 02_query_data.sql
@@ -1,1 +1,7 @@
-
+SELECT
+    SUM(valor) FILTER (WHERE categoria = 'A') AS suma_A,
+    SUM(valor) FILTER (WHERE categoria = 'B') AS suma_B,
+    SUM(valor) FILTER (WHERE categoria = 'C') AS suma_C,
+    SUM(valor) FILTER (WHERE categoria = 'D') AS suma_D
+FROM
+    ;
