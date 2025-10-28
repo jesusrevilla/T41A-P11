db05e0c2510f16c1df527d1f41bdad259e86a755
Jpablo179804 <179804@upslp.edu.mx>
2025-10-27 18:06:34 -0600

    Update 02_query_data.sql

--- 02_query_data.sql
+++ 02_query_data.sql
@@ -9,10 +9,19 @@
 --ORDER BY categoria;
 
 
-SELECT 
-    'suma' as medida,
-    SUM(CASE WHEN categoria = 'A' THEN valor ELSE 0 END) as A,
-    SUM(CASE WHEN categoria = 'B' THEN valor ELSE 0 END) as B,
-    SUM(CASE WHEN categoria = 'C' THEN valor ELSE 0 END) as C,
-    SUM(CASE WHEN categoria = 'D' THEN valor ELSE 0 END) as D
-FROM datos;
+--SELECT 
+--    'suma' as medida,
+--    SUM(CASE WHEN categoria = 'A' THEN valor ELSE 0 END) as A,
+--    SUM(CASE WHEN categoria = 'B' THEN valor ELSE 0 END) as B,
+--    SUM(CASE WHEN categoria = 'C' THEN valor ELSE 0 END) as C,
+--    SUM(CASE WHEN categoria = 'D' THEN valor ELSE 0 END) as D
+--FROM datos;
+
+
+SELECT
+    SUM(valor) FILTER (WHERE categoria = 'A') AS suma_A,
+    SUM(valor) FILTER (WHERE categoria = 'B') AS suma_B,
+    SUM(valor) FILTER (WHERE categoria = 'C') AS suma_C,
+    SUM(valor) FILTER (WHERE categoria = 'D') AS suma_D
+FROM
+    datos;
