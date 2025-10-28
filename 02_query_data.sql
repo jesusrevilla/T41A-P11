483ab3d99be4eed9d5b17861c28ac63bade5420c
Yovana-Palencia <174197@upslp.edu.mx>
2025-10-27 19:28:19 -0600

    Update 02_query_data.sql

--- 02_query_data.sql
+++ 02_query_data.sql
@@ -1,16 +1,1 @@
---Ver los primeros registros
-SELECT * FROM datos LIMIT 10;
-
---Promedio de valor por categoría
-SELECT categoria, ROUND(AVG(valor), 2) AS promedio_valor
-FROM datos
-GROUP BY categoria;
-
---Total general
-SELECT SUM(valor) AS total_general FROM datos;
-
---Cantidad de registros por categoría
-SELECT categoria, COUNT(*) AS total_registros
-FROM datos
-GROUP BY categoria;
 
