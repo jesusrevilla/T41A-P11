37ea7e3cfffd86528715affe25f9cd0404187fdc
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-10-27 19:20:53 -0600

    feature/182483

--- 02_query_data.sql
+++ 02_query_data.sql
@@ -1,1 +1,16 @@
+CREATE TEMP TABLE datos_csv (
+    id INTEGER,
+    categoria TEXT,
+    valor NUMERIC,
+    fecha DATE
+);
+
+\copy datos_csv FROM 'datos_agregacion_transformacion.csv' DELIMITER ',' CSV HEADER;
+
+INSERT INTO datos (id, categoria, valor, fecha)
+SELECT id, categoria, valor, fecha
+FROM datos_csv;
+
+SELECT COUNT(*) AS total_registros FROM datos;
+SELECT * FROM datos LIMIT 10;
 
