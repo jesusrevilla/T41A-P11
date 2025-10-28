2c7d1dea9ffb471032ce83f4db8bef1e409a9e1d
David Vidales <vidalesrmzdavid@gmail.com>
2025-10-28 08:35:55 -0600

    Update 02_query_data.sql

--- 02_query_data.sql
+++ 02_query_data.sql
@@ -1,1 +1,10 @@
+SELECT
+    fecha,
+    MAX(CASE WHEN categoria = 'A' THEN valor END) AS A,
+    MAX(CASE WHEN categoria = 'B' THEN valor END) AS B,
+    MAX(CASE WHEN categoria = 'C' THEN valor END) AS C,
+    MAX(CASE WHEN categoria = 'D' THEN valor END) AS D
+FROM datos
+GROUP BY fecha
+ORDER BY fecha;
 
