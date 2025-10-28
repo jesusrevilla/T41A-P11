667c0e1466668e3e02549f7331e81b73dbe6dea6
Yovana-Palencia <174197@upslp.edu.mx>
2025-10-27 19:27:32 -0600

    Update 03_insertar_datos.sql

--- 03_insertar_datos.sql
+++ 03_insertar_datos.sql
@@ -1,1 +1,1 @@
-\copy datos FROM 'datos_agregacion_transformacion.csv' DELIMITER ',' CSV HEADER;
+\copy datos(id, categoria, valor, fecha) FROM 'datos_agregacion_transformacion.csv' WITH (FORMAT CSV, HEADER TRUE);
