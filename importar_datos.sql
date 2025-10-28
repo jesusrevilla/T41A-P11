d021f6e491f51842ae520e69c2042d8ea09ebbb8
Coral024 <202579467+Coral024@users.noreply.github.com>
2025-10-27 19:13:07 -0600

    Update importar_datos.sql

--- importar_datos.sql
+++ importar_datos.sql
@@ -1,5 +1,2 @@
-COPY datos(id, categoria, valor, fecha)
-FROM 'datos_agregacion_transformacion.csv'
-DELIMITER ','
-CSV HEADER;
+\copy datos(id, categoria, valor, fecha) FROM 'datos_agregacion_transformacion.csv' DELIMITER ',' CSV HEADER;
 --sube archivo
