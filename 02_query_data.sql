fbee0229cc7e2ed1c99b3016e132d92e453edea7
rafaelSanchez4330 <rafaelsanchez9924@gmail.com>
2025-10-27 19:44:31 -0600

    Update 02_query_data.sql

--- 02_query_data.sql
+++ 02_query_data.sql
@@ -1,1 +1,13 @@
 
+/copy  datos FROM 'datos_agregacion_transformacion.csv' DELIMITER ',' CSV HEADER;
+
+CREATE EXTENSION IF NOT EXISTS tablefunc;
+SELECT *
+FROM crosstab(
+    SELECT SUM(valor)
+    FROM datos
+    GROUP BY categoria;
+) AS ct(A NUMERIC, B NUMERIC, C NUMERIC, D NUMERIC);
+
+
+
