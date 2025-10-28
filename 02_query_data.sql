e23be548ad57ca686acb3a097f4ce7c1a9465ec0
patymtz17 <181730@upslp.edu.mx>
2025-10-27 19:12:39 -0600

    Update 02_query_data.sql

--- 02_query_data.sql
+++ 02_query_data.sql
@@ -1,1 +1,16 @@
+\COPY datos(id, categoria, valor, fecha)
+FROM 'C:/Users/tu_usuario/T41A-P11/datos_agregacion_transformacion.csv'
+DELIMITER ','
+CSV HEADER;
 
+SELECT categoria, SUM(valor) AS total_valor
+FROM datos
+GROUP BY categoria
+ORDER BY categoria;
+
+SELECT
+    SUM(valor) FILTER (WHERE categoria = 'A') AS A,
+    SUM(valor) FILTER (WHERE categoria = 'B') AS B,
+    SUM(valor) FILTER (WHERE categoria = 'C') AS C,
+    SUM(valor) FILTER (WHERE categoria = 'D') AS D
+FROM datos;
