e7a785691d8086f4724c394377382016d4fef878
Jpablo179804 <179804@upslp.edu.mx>
2025-10-27 18:07:51 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,6 +1,8 @@
-CREATE TABLE IF NOT EXISTS datos (
-        id INTEGER PRIMARY KEY,
-        categoria TEXT,
-        valor NUMERIC,
-        fecha DATE
-    );
+--CREATE TABLE IF NOT EXISTS datos (
+--        id INTEGER PRIMARY KEY,
+--        categoria TEXT,
+--        valor NUMERIC,
+--        fecha DATE
+--    );
+
+\copy datos(id, categoria, valor, fecha) FROM 'datos_agregacion_transformacion.csv' WITH (FORMAT CSV, HEADER TRUE);
