9d4e9b3d7238a9f7477343ed63cbe4b5d0de72ff
tacodepastor25 <125671857+tacodepastor25@users.noreply.github.com>
2025-10-27 19:28:24 -0600

    Update 02_query_data.sql

--- 02_query_data.sql
+++ 02_query_data.sql
@@ -5,3 +5,4 @@ SELECT
     SUM(valor) FILTER (WHERE categoria = 'D') AS suma_D
 FROM
     datos;
+\copy datos(id, categoria, valor, fecha) FROM 'datos_agregacion_transformacion.csv' WITH (FORMAT CSV, HEADER TRUE);
