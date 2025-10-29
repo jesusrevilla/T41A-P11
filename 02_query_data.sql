495baa8971a13221f5fd3b4d0d8a9022a3f191cf
Emmanuel  Hernandez Mendez <179169@upslp.edu.mx>
2025-10-28 19:23:40 -0600

    Feature/179169

--- 02_query_data.sql
+++ 02_query_data.sql
@@ -1,7 +1,6 @@
 SELECT
-    SUM(valor) FILTER (WHERE categoria = 'A') AS suma_A,
-    SUM(valor) FILTER (WHERE categoria = 'B') AS suma_B,
-    SUM(valor) FILTER (WHERE categoria = 'C') AS suma_C,
-    SUM(valor) FILTER (WHERE categoria = 'D') AS suma_D
-FROM
-    datos;
+  SUM(CASE WHEN categoria = 'A' THEN valor ELSE 0 END) AS A,
+  SUM(CASE WHEN categoria = 'B' THEN valor ELSE 0 END) AS B,
+  SUM(CASE WHEN categoria = 'C' THEN valor ELSE 0 END) AS C,
+  SUM(CASE WHEN categoria = 'D' THEN valor ELSE 0 END) AS D
+FROM datos;
