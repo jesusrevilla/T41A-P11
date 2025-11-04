5e80a14760a51d058562275cdefbfa5f1d1baadc
JuanpiMarinD <126593527+JuanpiMarinD@users.noreply.github.com>
2025-11-03 19:42:04 -0600

    Update 02_query_data.sql

--- 02_query_data.sql
+++ 02_query_data.sql
@@ -1,7 +1,14 @@
-SELECT * FROM datos;
+WITH sumas AS (
+    SELECT 
+        categoria,
+        SUM(valor) AS total_valor
+    FROM datos
+    GROUP BY categoria
+)
 
-SELECT COUNT(*) AS total_registros FROM datos;
-
-SELECT categoria, AVG(valor) AS promedio_valor
-FROM datos
-GROUP BY categoria;
+SELECT
+    MAX(CASE WHEN categoria = 'A' THEN total_valor END) AS A,
+    MAX(CASE WHEN categoria = 'B' THEN total_valor END) AS B,
+    MAX(CASE WHEN categoria = 'C' THEN total_valor END) AS C,
+    MAX(CASE WHEN categoria = 'D' THEN total_valor END) AS D
+FROM sumas;
